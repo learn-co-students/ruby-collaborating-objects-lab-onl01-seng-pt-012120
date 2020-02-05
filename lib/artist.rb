8220003f1499c259f4afa53af91ebfc66515bed0
Dan Foley <dan@dan-foley.com>
2020-02-05 01:25:19 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+require "pry"
+
+
+class Artist
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def self.find_or_create_by_name(name)
+    # if Artist.all
+    binding.pry
+  end
+
+end
