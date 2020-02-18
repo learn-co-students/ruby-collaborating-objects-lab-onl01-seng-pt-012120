cf41d352c36005eab3fa238659d2c420e588b94f
Derric Fragosa <fderric@gmail.com>
2020-02-18 21:09:59 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,6 +14,6 @@ class MP3Importer
   end 
     
   def import
-    filename_list.each {|filenames| Song.new_by_filename(filename_list)}
+    self.files.each {|file| Song.new_by_filename(path)}
   end
 end 
